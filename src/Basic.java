671f988ffd363275a976187d5ccfce4533245a4f
Saugat Thapa <saugatthapa2073@gmail.com>
2025-02-07 00:21:00 -0600

    Hello World

---
+++ src/Basic.java
@@ -0,0 +1,12 @@
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming");
+
+        // add, subtract, multiply
+        int x = 10;
+        int y = 20;
+        System.out.println("Sum: " + (x+y));
+        System.out.println("Sub: " + (x-y));
+        System.out.println("Multiply: " + (x*y));
+    }
+}
