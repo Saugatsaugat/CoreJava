7f6c027d827d753e4709524a35978dd858d49968
Saugat Thapa <saugatthapa2073@gmail.com>
2025-02-12 00:10:33 -0600

    Local Inner class example added.

---
+++ src/pack/advancejava/Innnerclass/LocalInnerClass.java
@@ -0,0 +1,24 @@
+package pack.advancejava.Innnerclass;
+
+import java.util.Scanner;
+
+public class LocalInnerClass {
+    public void display(){
+        class InnerClass {
+            public void display(){
+                System.out.println("How old are you?");
+                Scanner sc = new Scanner(System.in);
+                int age = sc.nextInt();
+                System.out.println("I am "+age+" years old");
+            }
+        }
+        InnerClass inner = new InnerClass();
+        inner.display();
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+        LocalInnerClass local = new LocalInnerClass();
+        local.display();
+    }
+}
