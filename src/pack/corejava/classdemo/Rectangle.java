e92884b4619d9d5c716b5896da9fb4067a57933e
Saugat Thapa <saugatthapa2073@gmail.com>
2025-05-12 23:28:15 -0500

    class and object example with user input

--- src/pack/corejava/classdemo/Rectangle.java
+++ src/pack/corejava/classdemo/Rectangle.java
@@ -1,21 +1,34 @@
 package pack.corejava.classdemo;
 
-public class Rectangle {
-    int length;
-    int breadth;
+import java.util.Scanner;
 
-    public void getData(int x, int y){
+public class Rectangle {
+    // members
+    int length; // instance variable
+    int breadth; // instance variable
+
+
+    // defining parametrized methods
+    public void getData(int x, int y) {
         length = x;
         breadth = y;
     }
 
-    public int getArea(){
+    public int getArea() {
         return length * breadth;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Rectangle obj1 = new Rectangle();
-        obj1.getData(4, 2);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the length of the rectangle: ");
+        int length = sc.nextInt();
+        System.out.println("Enter the breadth of the rectangle: ");
+        int breadth = sc.nextInt();
+
+//        obj1.getData(4, 2);
+        obj1.getData(length, breadth);
         int area = obj1.getArea();
         System.out.println("The area of the rectangle is " + area);
     }
