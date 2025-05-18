6c3665ab2e740fe5fd8f0f696ca50dec3e366de8
Saugat Thapa <saugatthapa2073@gmail.com>
2025-05-18 12:57:55 -0500

    aggregation example added

---
+++ src/pack/advancejava/Aggregation.java
@@ -0,0 +1,46 @@
+/*
+Aggregation -> "HAS - A" relationship
+Inheritance -> "IS - A" relationship
+ */
+
+package pack.advancejava;
+
+class Address{
+    String street, city, state, zip;
+    public Address(String street, String city, String state, String zip){
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+
+        display();
+    }
+
+    // display the full address
+    public void display(){
+        System.out.println("Full address: " + street + ", " + city + ", " + state + ", " + zip);
+    }
+}
+
+class Employee{
+    String fullname;
+    Address address;
+
+    public Employee(String fullname, Address address){
+        this.fullname = fullname;
+        this.address = address;
+
+        display();
+    }
+
+    public void display(){
+        System.out.println("Full Name: " + fullname + "\nAddress: " + address.street + ", " + address.city +", " + address.state + ", " + address.zip);
+    }
+}
+
+public class Aggregation {
+    public static void main(String[] args){
+        Address add = new Address("123 Abc Avenue", "New York", "NY", "10003");
+        Employee em = new Employee("Mr. Thapa", add);
+    }
+}
