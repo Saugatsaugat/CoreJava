f9e4955e08e5bb78c7ae895edae4295b83db437a
Saugat Thapa <saugatthapa2073@gmail.com>
2025-05-18 19:53:14 -0500

    Copying file1 contents to file 2

---
+++ src/pack/advancejava/FileIO/CopyContent.java
@@ -0,0 +1,37 @@
+package pack.advancejava.FileIO;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyContent {
+    public static void main(String[] args) throws IOException {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        try{
+            String rootDir = System.getProperty("user.dir"); // this will gives you the working directory
+//            System.out.println(rootDir);
+            in = new FileInputStream(rootDir + "/src/staticfiles/test1.txt");
+            out = new FileOutputStream(rootDir + "/src/staticfiles/test2.txt");
+
+            int c;
+            while((c = in.read()) != -1){
+                out.write(c);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if(in != null){
+                in.close();
+            }
+            if (out != null){
+                out.close();
+            }
+        }
+    }
+
+}
