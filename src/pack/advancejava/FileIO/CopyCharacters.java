4625d17bfffcabc779aa1eb484f5a614e5e713b5
Saugat Thapa <saugatthapa2073@gmail.com>
2025-05-18 21:17:12 -0500

    renamed class from CopyStream to CopyCharacters

--- src/pack/advancejava/FileIO/CopyCharacters.java
+++ src/pack/advancejava/FileIO/CopyCharacters.java
@@ -4,7 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class CopyStream {
+public class CopyCharacters {
     public static void main(String[] args) throws IOException {
         FileReader in = null;
         FileWriter out = null;
