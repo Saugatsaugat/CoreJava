f5dc3e13f6cdabbea43b308c54f8a11857ac7b58
Saugat Thapa <saugatthapa2073@gmail.com>
2025-05-20 16:09:48 -0500

    added an example of reversing a string

---
+++ src/pack/extra/StringReverse.java
@@ -0,0 +1,18 @@
+package pack.extra;
+
+import java.util.Arrays;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        String str = "ReverseString";
+//        StringBuilder reversed = new StringBuilder(str);
+//        reversed.reverse();
+//        System.out.println(reversed);
+
+        String reverseStr = "";
+        for(int i =0; i<str.length(); i++){
+            reverseStr = str.charAt(i) + reverseStr;
+        }
+        System.out.println(reverseStr);
+    }
+}
