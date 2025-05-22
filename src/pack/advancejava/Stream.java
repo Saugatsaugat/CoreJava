950b0f29006831c71cd5f44c1cd40a017bd8613a
Saugat Thapa <saugatthapa2073@gmail.com>
2025-05-21 19:25:07 -0500

    Added example of stream API for filter function

---
+++ src/pack/advancejava/Stream.java
@@ -0,0 +1,43 @@
+/*
+Stream:
+ - introduced in java8
+ - Lazy
+ - Doesn't store data
+ - is used with the collections( list, map, set)
+ */
+
+package pack.advancejava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream {
+    public static void main(String[] args) {
+        String[] cities = {"New York", "Chicago", "Denver", "Atlanta", "Dallas"};
+        // converting the array to list so that we can use Stream API
+        List<String> arrayList = Arrays.stream(cities).map(String::toUpperCase).collect(Collectors.toList());
+        for (String str : arrayList) {
+            System.out.print(str + "\t");
+        }
+
+        // creating list
+        ArrayList<String> states = new ArrayList<>();
+        states.add("Virginia");
+        states.add("Texas");
+        states.add("Maryland");
+        states.add("Maine");
+        states.add("Washington");
+        states.add("Colorado");
+
+        List<String> filteredStateByStartingWithM = states.stream()
+                .filter(x -> x.startsWith("M"))
+                .collect(Collectors.toList());
+        System.out.println("/n Filtering the list starting with letter M: \n");
+        for(String str: filteredStateByStartingWithM){
+            System.out.print(str + "\t");
+        }
+
+    }
+}
