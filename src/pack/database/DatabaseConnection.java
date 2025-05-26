343eeb4dfb632bffb860ad17dada4a6dc0f2db15
Saugat Thapa <saugatthapa2073@gmail.com>
2025-05-26 15:00:26 -0500

    made the DatabaseConnection class to return Connection object and added code to insert and fetch data from database

--- src/pack/database/DatabaseConnection.java
+++ src/pack/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     static Connection con = null;
 
-    public static void main(String[] args) throws SQLException {
+    public static Connection getDatabaseConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Loaded MySQL JDBC");
@@ -18,19 +18,18 @@ public class DatabaseConnection {
             String user = "saugat";
             String password = "";
 
-            String jdbcUrl = "jdbc:mysql://"+host+":"+port+"/"+db;
+            String jdbcUrl = "jdbc:mysql://" + host + ":" + port + "/" + db;
             con = DriverManager.getConnection(jdbcUrl, user, password);
             System.out.println("Connection Established");
 
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
-        } catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally{
-            con.close();
         }
-    }
 
+        return con;
+    }
 }
---
+++ src/pack/database/InsertData.java
@@ -0,0 +1,37 @@
+package pack.database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import pack.database.DatabaseConnection;
+
+public class InsertData {
+    static Connection con = null;
+    static Statement statement = null;
+    static ResultSet resultSet = null;
+
+    public static void main(String[] args) throws SQLException {
+        con = DatabaseConnection.getDatabaseConnection();
+        try {
+            String insertData = "INSERT INTO employee_db.employee(firstname, middlename, lastname, age) VALUES('Saugat', ' ', 'DEVP', 23)";
+            statement = con.createStatement();
+            statement.executeUpdate(insertData);
+            System.out.println("Data inserted successfully");
+
+            // fetch
+            String selectQuery = "SELECT * from employee;";
+            resultSet = statement.executeQuery(selectQuery);
+            while (resultSet.next()) {
+                String fullname = resultSet.getNString("firstname") + (resultSet.getNString("middlename").isEmpty() ? " " + resultSet.getNString("lastname") : (resultSet.getNString("middlename") + " " + resultSet.getNString("lastname")));
+                int age = resultSet.getInt("age");
+                System.out.println("\nFullname: " + fullname + "\nAge: " + age);
+            }
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
